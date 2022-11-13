06b542eb5e4e4cec4fab275af8ada907b9fb835b
Dmitry Khrapov <hrapovd@gmail.com>
2022-11-13 20:33:10 +0300

    Fix tests

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,14 +1,17 @@
 package config
 
 import (
+	"errors"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/caarlos0/env/v6"
 )
 
 type environ struct {
-	PollInterval   int    `env:"POLL_INTERVAL" envDefault:"2"`
-	ReportInterval int    `env:"REPORT_INTERVAL" envDefault:"10"`
+	PollInterval   string `env:"POLL_INTERVAL" envDefault:"2s"`
+	ReportInterval string `env:"REPORT_INTERVAL" envDefault:"10s"`
 	Address        string `env:"ADDRESS" envDefault:"localhost:8080"`
 }
 
@@ -20,10 +23,17 @@ type Config struct {
 }
 
 func (cfg *Config) NewAgent() error {
+	var err error
 	var envs environ
-	err := env.Parse(&envs)
-	cfg.PollInterval = time.Duration(envs.PollInterval) * time.Second
-	cfg.ReportInterval = time.Duration(envs.ReportInterval) * time.Second
+	if err = env.Parse(&envs); err != nil {
+		return err
+	}
+	if cfg.PollInterval, err = parseInterval(envs.PollInterval); err != nil {
+		return err
+	}
+	if cfg.ReportInterval, err = parseInterval(envs.ReportInterval); err != nil {
+		return err
+	}
 	cfg.ServerAddress = envs.Address
 	cfg.RetryCount = 3
 	return err
@@ -36,13 +46,19 @@ func (cfg *Config) NewServer() error {
 	return err
 }
 
-var AgentConfig = Config{
-	PollInterval:   2 * time.Second,
-	ReportInterval: 10 * time.Second,
-	RetryCount:     3,
-	ServerAddress:  "127.0.0.1",
-}
-
-var ServerConfig = Config{
-	ServerAddress: "127.0.0.1",
+func parseInterval(interval string) (time.Duration, error) {
+	value := make([]string, 0)
+	for _, ch := range interval {
+		value = append(value, string(ch))
+	}
+	switch value[len(value)-1] {
+	case "s":
+		seconds, err := strconv.Atoi(strings.Join(value[:len(value)-1], ""))
+		return time.Duration(seconds) * time.Second, err
+	case "m":
+		minutes, err := strconv.Atoi(strings.Join(value[:len(value)-1], ""))
+		return time.Duration(minutes) * time.Minute, err
+	default:
+		return time.Second, errors.New("wrong interval format")
+	}
 }
